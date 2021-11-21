d8a31ef4ff23a988c69c1ee58f1299a1491cb960
Ben Hong <ben@bencodezen.io>
2021-11-21 17:06:41 -0500

    feature: add basic vue router setup

---
+++ playground/src/pages/Home.vue
@@ -0,0 +1,11 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <main>
+    <h1>Home Page</h1>
+  </main>
+</template>
+
+<style></style>
---
+++ playground/src/routes.js
@@ -0,0 +1,10 @@
+import HomePage from './pages/Home.vue'
+
+const pageRoutes = [
+  {
+    path: '/',
+    component: HomePage
+  }
+]
+
+export default pageRoutes
