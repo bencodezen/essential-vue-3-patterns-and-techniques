081eecaef6165ecc23e5f101cbd8de9d5f15343b
Ben Hong <ben@bencodezen.io>
2021-11-21 17:19:52 -0500

    feature: establish basic layout of playground

--- playground/src/pages/Home.vue
+++ playground/src/pages/Home.vue
@@ -3,9 +3,38 @@ export default {}
 </script>
 
 <template>
-  <main>
-    <h1>Home Page</h1>
-  </main>
+  <div class="default-grid">
+    <aside class="sidebar">
+      <h2>Navigation</h2>
+    </aside>
+    <main class="main-content">
+      <h1>Home Page</h1>
+    </main>
+  </div>
 </template>
 
-<style></style>
+<style>
+:root {
+  --vue-blue: #34495e;
+  --vue-green: #41b883;
+}
+
+*,
+html,
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.default-grid {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  min-height: 100vh;
+}
+
+.sidebar {
+  background-color: var(--vue-blue);
+  color: white;
+}
+</style>
